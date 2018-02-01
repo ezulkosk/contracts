f2bd1676d609d574c3ecde74e23f04c2ae205e64
Ngan Pham <ngan@users.noreply.github.com>
2018-02-01 13:04:59 -0800

    Remove assert and emit Transfer events

--- contracts/InkProtocol.sol
+++ contracts/InkProtocol.sol
@@ -15,28 +15,24 @@ contract InkProtocol is InkProtocolCore {
   */
 
   function InkProtocol() public {
-    uint256 allocated;
-
-    // Allocate 32% to vesting contract for Ink distribution/network incentives.
-    balances[__address1__] = 160000000000000000000000000;
-    allocated = allocated.add(balanceOf(__address1__));
-
-    // Allocate 32% to vesting contract for Listia Inc.
-    balances[__address2__] = 160000000000000000000000000;
-    allocated = allocated.add(balanceOf(__address2__));
-
-    // Allocate 6% to wallet for Listia Marketplace credit conversion.
-    balances[__address3__] = 30000000000000000000000000;
-    allocated = allocated.add(balanceOf(__address3__));
-
-    // Allocate to wallet for token sale distribution.
-    balances[__address4__] = 130374026302104500000000000;
-    allocated = allocated.add(balanceOf(__address4__));
-
     // Burn unsold tokens due to token sale hard cap.
     uint256 burnedSupply = 19625973697895500000000000;
     totalSupply_ = totalSupply_.sub(burnedSupply);
 
-    assert(totalSupply_ == allocated);
+    // Allocate 32% to vesting contract for Ink distribution/network incentives.
+    balances[__address1__] = 160000000000000000000000000;
+    Transfer(address(0), __address1__, balanceOf(__address1__));
+
+    // Allocate 32% to vesting contract for Listia Inc.
+    balances[__address2__] = 160000000000000000000000000;
+    Transfer(address(0), __address2__, balanceOf(__address2__));
+
+    // Allocate 6% to wallet for Listia Marketplace credit conversion.
+    balances[__address3__] = 30000000000000000000000000;
+    Transfer(address(0), __address3__, balanceOf(__address3__));
+
+    // Allocate to wallet for token sale distribution.
+    balances[__address4__] = 130374026302104500000000000;
+    Transfer(address(0), __address4__, balanceOf(__address4__));
   }
 }
